6b8e512686af7a7103576525545f35974bf22460
Michael <80859948+bridgemami@users.noreply.github.com>
2021-12-28 06:36:30 +0000

    added cap

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+//useGeographic();
+
+//const place = [28.9784, 41.0082];
 var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([35.2433, 38.9637]),
-          zoom: 4
-        })
-      });
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([28.9784, 41.0082]),
+      zoom: 4
+    })
+  });
+  var layer = new ol.layer.Vector({
+     source: new ol.source.Vector({
+         features: [
+             new ol.Feature({
+                 geometry: new ol.geom.Point(ol.proj.fromLonLat([28.9784, 41.0082]))
+             }),
+             new ol.Feature({
+                 geometry: new ol.geom.Point(ol.proj.fromLonLat([34.846306, 38.640388]))
+             })
+         ]
+     })
+ });
+ map.addLayer(layer);
+ // change mouse cursor when over marker
